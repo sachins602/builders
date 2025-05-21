c48a14695d8c22fbd18262ab8ae7466a0869ae72
sachins602 <sachinsapkota4@gmail.com>
2025-05-21 17:19:22 -0400

    add toronto topojson

---
+++ src/app/_components/map.tsx
@@ -0,0 +1,8 @@
+export default function WholeMap() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <h1 className="text-2xl text-white">Map</h1>
+      <div className="w-full max-w-3xl"></div>
+    </div>
+  );
+}
