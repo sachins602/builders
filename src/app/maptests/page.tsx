db822b0bff887780d42a334d8c10486e0e1113a9
sachins602 <sachinsapkota4@gmail.com>
2025-06-06 16:49:14 -0400

    setup

---
+++ src/app/maptests/page.tsx
@@ -0,0 +1,9 @@
+export default function MapTests() {
+  console.log("MapTest component loaded");
+  return (
+    <div>
+      <h1>Map Test</h1>
+      <p>This is a placeholder for the map test component.</p>
+    </div>
+  );
+}
