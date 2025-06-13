ea1199ec0a786c163fc53fd9b5c22e3e7d7432cc
sachins602 <sachinsapkota4@gmail.com>
2025-06-13 00:30:19 -0400

    refactor header component

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,5 +1,4 @@
 // External libraries
-import Link from "next/link";
 import Image from "next/image";
 
 // Internal modules
@@ -7,21 +6,68 @@ import { auth } from "~/server/auth";
 
 // UI components
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import NavMenu from "./navMenu";
-import UserIcon from "./userIcon";
-
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "./ui/navigation-menu";
+import { Bars4Icon } from "@heroicons/react/24/outline";
 
 export default async function Header() {
 
-
+  const session = await auth();
   return (
     <header className="flex flex-row items-center justify-between text-black p-1">
       <div className="flex flex-row items-center justify-between w-26">
-        {/*Navigation Menu*/}
-        {NavMenu()}
+      <NavigationMenu className="z-[10000]">
+          <NavigationMenuList>
+            <NavigationMenuItem>
 
+              {/* Hamburger Menu Icon*/}
+              <NavigationMenuTrigger>
+                <Bars4Icon className="h-full" />
+              </NavigationMenuTrigger>
+
+              <NavigationMenuContent className="w-[300px]">
+
+                {/* Navigation Links */}
+                <NavigationMenuLink className="w-[300px]">My Builds & Remixes</NavigationMenuLink>
+                <NavigationMenuLink>My Likes</NavigationMenuLink>
+                <NavigationMenuLink>Manage Community Organizations</NavigationMenuLink>
+
+                {/*Check for user status - user profile*/}
+                {session && (
+                    <NavigationMenuLink href="/profile">
+                        Edit Profile
+                    </NavigationMenuLink>
+                )}
+
+                {/* Check for user status - login/logout */}
+                <NavigationMenuLink href={session ? "/api/auth/signout" : "/api/auth/signin"}>
+                  {session ? (
+                      "Logout - " + session.user.name
+                  ) : (
+                      "Log in"
+                  )}
+
+                </NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
         {/*User Icon*/}
-        {UserIcon()}
+        {session && (
+            <Avatar>
+                <AvatarImage
+                    src={session.user.image ?? undefined}
+                    style={{ filter: "grayscale(100%)" }}
+                />
+                <AvatarFallback>img</AvatarFallback>
+            </Avatar>
+        )}
       </div>
 
       {/*Logo*/}
