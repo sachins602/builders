bc7bce6f9c6f1abfa31b76ca9f0816ea835093d4
sachins602 <sachinsapkota4@gmail.com>
2025-07-18 03:27:53 -0400

    Implement session-based authorization in AdminPage component
    - Added authentication check to ensure only users with the 'admin' role can access the AdminPage.
    - Displayed a message for unauthorized users, enhancing security and user experience in the admin dashboard.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,10 @@
+import { auth } from "~/server/auth";
 import AdminCharts from "../_components/admin-charts";
 
 export default async function AdminPage() {
+  const session = await auth();
+  if (!session || session.user.role !== "admin") {
+    return <div>Sorry, Looks like you are not allowed be here!</div>;
+  }
   return <AdminCharts />;
 }
