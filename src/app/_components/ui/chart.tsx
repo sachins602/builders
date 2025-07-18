980836a1297e01f2f97967548d94ac71f8934595
sachins602 <sachinsapkota4@gmail.com>
2025-07-18 03:57:38 -0400

    Update Chart component to disable specific ESLint rules for flexibility in type handling
    - Added ESLint disable comments to allow for more flexible type assertions and member access in the Chart component.
    - This change aims to facilitate development while maintaining focus on functionality.

--- src/app/_components/ui/chart.tsx
+++ src/app/_components/ui/chart.tsx
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 "use client";
 
 import * as React from "react";
