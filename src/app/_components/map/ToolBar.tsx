beb804292a8aba228f3e43dc153d3bb4976d617d
sachins602 <sachinsapkota4@gmail.com>
2025-07-24 03:09:39 -0400

    Refactor ToolBar component for improved layout and styling
    - Updated button layout in ToolBar to use a flex container with gap for better spacing.
    - Adjusted button sizes and removed unnecessary classes for a cleaner design.
    - Enhanced the Search, Build, and Edit buttons for improved visual consistency.

--- src/app/_components/map/ToolBar.tsx
+++ src/app/_components/map/ToolBar.tsx
@@ -13,26 +13,21 @@ export function ToolBar({
   existingImageId,
 }: ToolBarProps) {
   return (
-    <div className="flex w-full flex-row justify-center">
-      <Button
-        variant="secondary"
-        onClick={onSearchClick}
-        className="m-2 flex h-24 w-24 flex-col p-2"
-      >
-        <Search className="m-2 h-32 w-32 scale-250" />
+    <div className="flex w-full flex-row justify-center gap-4">
+      <Button variant="secondary" onClick={onSearchClick}>
+        <Search className="m-2 h-28 w-28 scale-250" />
         <span>Search</span>
       </Button>
 
       {showBuildEditButtons && (
-        <>
+        <div className="flex flex-row gap-4">
           <Button
             variant="secondary"
             onClick={() => {
               window.location.href = "/create";
             }}
-            className="m-2 flex h-24 w-24 flex-col p-2"
           >
-            <Hammer className="m-2 h-32 w-32 scale-250" />
+            <Hammer className="m-2 h-28 w-28 scale-250" />
             <p>Build</p>
           </Button>
 
@@ -45,12 +40,11 @@ export function ToolBar({
                 window.location.href = "/edit";
               }
             }}
-            className="m-2 flex h-24 w-24 flex-col p-2"
           >
-            <Edit className="m-2 h-32 w-32 scale-250" />
+            <Edit className="m-2 h-28 w-28 scale-250" />
             <p>Edit</p>
           </Button>
-        </>
+        </div>
       )}
     </div>
   );
