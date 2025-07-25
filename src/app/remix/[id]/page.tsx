ebb8c8db505e4189c7e7e1ed7a3f821961bbea9a
sachins602 <sachinsapkota4@gmail.com>
2025-07-25 17:09:02 -0400

    Add RemixPage component and user response retrieval logic
    - Introduced a new RemixPage component to display user responses based on the provided ID.
    - Implemented API logic to fetch user responses, including handling access control for shared and unshared responses.
    - Enhanced the user experience by integrating the CommunityPost component for displaying fetched posts.

---
+++ src/app/remix/[id]/page.tsx
@@ -0,0 +1,29 @@
+import { notFound } from "next/navigation";
+import { CommunityPost } from "~/app/_components/CommunityPost/CommunityPost";
+import { HydrateClient } from "~/trpc/server";
+import { api } from "~/trpc/server";
+
+interface RemixPageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function RemixPage({ params }: RemixPageProps) {
+  const resolvedParams = await params;
+  const post = await api.user.getUserResponseById({
+    responseId: parseInt(resolvedParams.id),
+  });
+
+  if (!post) return notFound();
+
+  console.log(post);
+
+  return (
+    <HydrateClient>
+      <div className="mx-auto max-w-2xl">
+        <CommunityPost post={post} />
+      </div>
+    </HydrateClient>
+  );
+}
